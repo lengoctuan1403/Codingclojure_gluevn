d9e856cea8f769b40d9f93dfc9c7812be7e34138
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2024-12-23 14:02:51 +0700

    initial commit a new branch

---
+++ src/coding/abc.clj
@@ -0,0 +1,13 @@
+(ns coding.abc)
+
+(defn print-my-name
+  []
+(println "My name is Le Ngoc Tuan"))
+
+(print-my-name)
+
+(defn print-your-name
+  [yourname]
+  (println "Your name is"yourname))
+
+(print-your-name "Phong")
